6068d77a02d70814ddc2927e86780f1a6caf1632
yjd <yunjianding@gmail.com>
2016-06-16 18:38:40 -0400

    Removed redundant MNIST class.

--- 1_nn_mnist_scipy.py
+++ 1_nn_mnist_scipy.py
@@ -2,25 +2,25 @@
 
 #Applies a 3-layer neural net to the MNIST handwritten digits
 
-from mnist import *
 from nn_scipy import SimpleSoftmaxNN
+import mnist
 import numpy as np
 from sys import stdout as cout
 
-training_labels = MNISTLabels("res/train-labels.idx1-ubyte", True)
-training_labels.split_validation_set()
+n_validation = 5000
 
-training_images = MNISTImages("res/train-images.idx3-ubyte", True)
-training_images.split_validation_set()
+n, labels = mnist.read_labels('res/train-labels.idx1-ubyte', True)
+n, width, height, images = mnist.read_images('res/train-images.idx3-ubyte')
+images = images.reshape([n, width * height])
 
-nn = SimpleSoftmaxNN([training_images.width * training_images.height, 140, 10], 2e-5)
+nn = SimpleSoftmaxNN([width * height, 140, 10], 2e-5)
 try:
   nn.optimize(
     50000,
     100,
     0.3,
-    training_images.get_images(),
-    training_labels.get_labels(),
+    images[n_validation:],
+    labels[n_validation:],
     True
   )
 except:
@@ -28,14 +28,15 @@ except:
 
 print('Validating')
 
-testing_labels = MNISTLabels('res/t10k-labels.idx1-ubyte', True)
-testing_images = MNISTImages('res/t10k-images.idx3-ubyte', True)
+n, test_labels = mnist.read_labels('res/t10k-labels.idx1-ubyte', True)
+n, width, height, test_images = mnist.read_images('res/t10k-images.idx3-ubyte')
+test_images = test_images.reshape([n, width * height])
 
-r = nn.feed_forward(training_images.get_validation())
-validation = r.argmax(1) == training_labels.get_validation().argmax(1)
+r = nn.feed_forward(images[:n_validation])
+validation = r.argmax(1) == labels[:n_validation].argmax(1)
 
-r = nn.feed_forward(testing_images.get_images())
-test = r.argmax(1) == testing_labels.get_labels().argmax(1)
+r = nn.feed_forward(test_images)
+test = r.argmax(1) == test_labels.argmax(1)
 
-print('Validation accuracy: {0}%'.format(100. * np.sum(validation) / len(training_labels.get_validation())))
-print('Testing accuracy: {0}%'.format(100 * np.sum(test) / len(testing_labels.get_labels())))
+print('Validation accuracy: {0}%'.format(100. * np.sum(validation) / n_validation))
+print('Testing accuracy: {0}%'.format(100 * np.sum(test) / len(test_labels)))
--- mnist.py
+++ mnist.py
@@ -1,7 +1,7 @@
 import numpy as np
 import struct
 
-__all__ = ['MNISTImages', 'MNISTLabels', 'read_images', 'read_labels']
+__all__ = ['read_images', 'read_labels']
 
 def read_labels(filepath, one_hot=False):
   with open(filepath, 'rb') as f:
@@ -25,44 +25,3 @@ def read_images(filepath):
     images = np.fromfile(f, np.uint8).astype(np.float32) / 255.
 
   return n, w, h, images
-
-class MNISTImages:
-  def __init__(self, filepath, flat=False):
-    self.flat = flat
-    self.n, self.width, self.height, self.images = read_images(filepath)
-    self.validation = None
-
-    if flat:
-      self.images = self.images.reshape([self.n, self.width * self.height])
-    else:
-      self.images = self.images.reshape([self.n, self.width, self.height])
-
-  def split_validation_set(self, n = 5000):
-    if self.validation is None:
-      self.images, self.validation = self.images[:self.n - n], self.images[self.n - n:]
-
-    return self.images, self.validation
-
-  def get_images(self):
-    return self.images
-
-  def get_validation(self):
-    return self.validation
-
-class MNISTLabels:
-  def __init__(self, filepath, one_hot=False):
-    self.one_hot = one_hot
-    self.n, self.labels = read_labels(filepath, one_hot)
-    self.validation = None
-
-  def split_validation_set(self, n = 5000):
-    if self.validation is None:
-      self.labels, self.validation = self.labels[:self.n - n], self.labels[self.n - n:]
-
-    return self.labels, self.validation
-
-  def get_labels(self):
-    return self.labels
-
-  def get_validation(self):
-    return self.validation
